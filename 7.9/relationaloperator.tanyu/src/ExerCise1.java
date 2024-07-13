e8f1786cc4e18805879ed8a501ca1d341bb59645
yuyue0s <100188960+yuyue0s@users.noreply.github.com>
2024-07-13 23:00:08 +0800

    这是关于关系运算符和赋值运算符的学习记录和学习过程中写的一些代码

---
+++ 7.9/relationaloperator.tanyu/src/ExerCise1.java
@@ -0,0 +1,13 @@
+public class ExerCise1 {
+    public static void main(String[] args){
+        //演示关系运算符的使用
+        int a = 8;
+        int b = 9;
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a == b);
+        boolean flag = a > b;
+        System.out.println("flag=" + flag);
+    }
+}
