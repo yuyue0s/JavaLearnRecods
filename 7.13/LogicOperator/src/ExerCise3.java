9f59701cfceec236752da348fe2c5f7d9befb746
yuyue0s <100188960+yuyue0s@users.noreply.github.com>
2024-07-16 20:37:14 +0800

    Delete 7.13 directory

--- 7.13/LogicOperator/src/ExerCise3.java
+++
@@ -1,9 +0,0 @@
-public class ExerCise3 {
-    public static void main (String[]args){
-        System.out.println(60 > 20);//ture
-        System.out.println(!(60 > 20));//false
-    //^逻辑与或 两边不同为ture 否则false
-        boolean a = (1 < 5) ^ (3 > 2);
-        System.out.println("a=" + a);//false
-    }
-}
